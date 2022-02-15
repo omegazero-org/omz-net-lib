359a453047decefad03a2ab47687af3bbfbe48a1
user94729 <user94729@omegazero.org>
2022-02-15 18:59:41 +0100

    SSLUtil: Added Javadoc

--- net-common/main/java/org/omegazero/net/util/SSLUtil.java
+++ net-common/main/java/org/omegazero/net/util/SSLUtil.java
@@ -33,20 +33,54 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLServerSocketFactory;
 
+/**
+ * Contains utility methods for managing SSL/TLS objects and keys/certificates.
+ */
 public final class SSLUtil {
 
 	private SSLUtil() {
 	}
 
 
+	/**
+	 * Returns the default server socket factory.
+	 * 
+	 * @return The default server socket factory
+	 * @deprecated Use {@link ServerSocketFactory#getDefault()}
+	 */
 	public static ServerSocketFactory getServerSocketFactory() {
 		return ServerSocketFactory.getDefault();
 	}
 
+	/**
+	 * Creates a {@link SSLContext} with the "{@code TLS}" protocol from a Java key store stored in a file identified by <b>filename</b>.
+	 * 
+	 * @param filename    The key store file
+	 * @param password    The file password
+	 * @param keypassword The key password
+	 * @return The {@code SSLContext}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @see #getSSLContextFromKeyStore(String, String, String, String)
+	 * @see SSLContext#getInstance(String)
+	 */
 	public static SSLContext getSSLContextFromKeyStore(String filename, String password, String keypassword) throws GeneralSecurityException, IOException {
 		return SSLUtil.getSSLContextFromKeyStore("TLS", filename, password, keypassword);
 	}
 
+	/**
+	 * Creates a {@link SSLContext} from a Java key store stored in a file identified by <b>filename</b>.
+	 * 
+	 * @param protocol    The name of the protocol the {@code SSLContext} will be used for
+	 * @param filename    The key store file
+	 * @param password    The file password
+	 * @param keypassword The key password
+	 * @return The {@code SSLContext}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @see #getSSLContextFromKeyStore(String, String, String)
+	 * @see SSLContext#getInstance(String)
+	 */
 	public static SSLContext getSSLContextFromKeyStore(String protocol, String filename, String password, String keypassword) throws GeneralSecurityException, IOException {
 		if(filename == null)
 			throw new IllegalArgumentException("Tried to create a SSLContext but filename is null");
@@ -62,10 +96,33 @@ public final class SSLUtil {
 	}
 
 
+	/**
+	 * Creates a {@link SSLContext} with the "{@code TLS}" protocol from PEM-encoded key and certificate files.
+	 * 
+	 * @param keyFile  The key file
+	 * @param certFile The certificate file
+	 * @return The {@code SSLContext}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @see #getSSLContextFromPEM(String, String, String)
+	 * @see SSLContext#getInstance(String)
+	 */
 	public static SSLContext getSSLContextFromPEM(String keyFile, String certFile) throws GeneralSecurityException, IOException {
 		return SSLUtil.getSSLContextFromPEM("TLS", keyFile, certFile);
 	}
 
+	/**
+	 * Creates a {@link SSLContext} from PEM-encoded key and certificate files.
+	 * 
+	 * @param protocol The name of the protocol the {@code SSLContext} will be used for
+	 * @param keyFile  The key file
+	 * @param certFile The certificate file
+	 * @return The {@code SSLContext}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @see #getSSLContextFromPEM(String, String)
+	 * @see SSLContext#getInstance(String)
+	 */
 	public static SSLContext getSSLContextFromPEM(String protocol, String keyFile, String certFile) throws GeneralSecurityException, IOException {
 		if(keyFile == null || certFile == null)
 			throw new IllegalArgumentException("Tried to create a SSLContext but a filename is null");
@@ -85,15 +142,54 @@ public final class SSLUtil {
 		return sslContext;
 	}
 
+	/**
+	 * Creates a {@link SSLServerSocketFactory} from PEM-encoded key and certificate files.
+	 * 
+	 * @param keyFile  The key file
+	 * @param certFile The certificate file
+	 * @return The {@code SSLServerSocketFactory}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @deprecated Use {@link #getSSLContextFromPEM(String, String)} and {@link SSLContext#getServerSocketFactory()}
+	 */
+	@Deprecated
 	public static SSLServerSocketFactory getSecureServerSocketFactory(String keyFile, String certFile) throws GeneralSecurityException, IOException {
 		return getSSLContextFromPEM(keyFile, certFile).getServerSocketFactory();
 	}
 
+	/**
+	 * Creates a {@link SSLEngine} from PEM-encoded key and certificate files.
+	 * 
+	 * @param keyFile  The key file
+	 * @param certFile The certificate file
+	 * @return The {@code SSLEngine}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @deprecated Use {@link #getSSLContextFromPEM(String, String)} and {@link SSLContext#createSSLEngine()}
+	 */
+	@Deprecated
 	public static SSLEngine getSSLEngineWithPEM(String keyFile, String certFile) throws GeneralSecurityException, IOException {
 		return getSSLContextFromPEM(keyFile, certFile).createSSLEngine();
 	}
 
 
+	/**
+	 * Reads one or more encoded certificates from the given string <b>data</b> and returns the decoded bytes.
+	 * <p>
+	 * The given <b>data</b> is expected to contain one or more blocks of the following format:
+	 * 
+	 * <pre>
+	 * <code>
+	 * -----BEGIN CERTIFICATE-----
+	 * &lt;base64-encoded certificate data&gt;
+	 * -----END CERTIFICATE-----
+	 * </code>
+	 * </pre>
+	 * 
+	 * @param data The string data
+	 * @return The list of decoded certificate blocks
+	 * @throws IOException If the format is invalid
+	 */
 	public static List<byte[]> readCertificatePEM(String data) throws IOException {
 		Scanner scanner = new Scanner(data);
 		List<byte[]> certs = new LinkedList<>();
@@ -122,12 +218,29 @@ public final class SSLUtil {
 	}
 
 
+	/**
+	 * Reads a {@link PrivateKey} from the given <b>keyFile</b>.
+	 * 
+	 * @param keyFile The key file
+	 * @return The {@code PrivateKey}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 * @see PrivateKeyReader#read(String)
+	 */
 	public static PrivateKey loadPrivateKeyFromPEM(String keyFile) throws GeneralSecurityException, IOException {
 		String keyFileData = new String(Files.readAllBytes(Paths.get(keyFile)));
 		PrivateKey key = PrivateKeyReader.read(keyFileData);
 		return key;
 	}
 
+	/**
+	 * Reads a single {@link X509Certificate} from the given <b>certFile</b>.
+	 * 
+	 * @param certFile The certificate file
+	 * @return The {@code X509Certificate}
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 */
 	public static X509Certificate loadCertificateFromPEM(String certFile) throws GeneralSecurityException, IOException {
 		CertificateFactory cf = CertificateFactory.getInstance("X.509");
 		String certFileData = new String(Files.readAllBytes(Paths.get(certFile)));
@@ -139,6 +252,14 @@ public final class SSLUtil {
 		return (X509Certificate) cf.generateCertificate(cdstream);
 	}
 
+	/**
+	 * Reads one or more {@link X509Certificate} from the given <b>certFile</b>.
+	 * 
+	 * @param certFile The certificate file
+	 * @return The {@code X509Certificate}s
+	 * @throws GeneralSecurityException If an SSL error occurs
+	 * @throws IOException              If an IO error occurs while reading the file
+	 */
 	public static X509Certificate[] loadCertificatesFromPEM(String certFile) throws GeneralSecurityException, IOException {
 		CertificateFactory cf = CertificateFactory.getInstance("X.509");
 		String certFileData = new String(Files.readAllBytes(Paths.get(certFile)));
